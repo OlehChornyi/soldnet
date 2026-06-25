06dcb4da00c617285e0c370aa16c406a7de3ca23
oleh <phd.chornyi@gmail.com>
2026-06-25 12:32:51 +0300

    app_router

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:soldnet/app/app_router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -16,6 +18,57 @@ class App extends StatelessWidget {
       builder: (context, child) {
         return child!;
       },
+    );
+  }
+}
+
+class RedirectionPage extends ConsumerStatefulWidget {
+  const RedirectionPage({super.key});
+
+  @override
+  ConsumerState<RedirectionPage> createState() => _RedirectionPageState();
+}
+
+class _RedirectionPageState extends ConsumerState<RedirectionPage> {
+  @override
+  void initState() {
+    super.initState();
+    _redirect();
+  }
+
+  void _redirect() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      // final userNotirier = ref.read(storeUserProvider.notifier);
+      // final token = await userNotirier.getTokenFromSharedPreferences();
+      // await Future.delayed(Duration(seconds: 2));
+      // if (token.isNotEmpty) {
+      //   router.go(ScreenPaths.home);
+      // } else {
+      //   router.go(ScreenPaths.signin);
+      // }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: AppColors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // SvgPicture.asset(
+            //   'assets/icons/common/pear.svg',
+            //   width: 96,
+            //   height: 96,
+            //   colorFilter: ColorFilter.mode(AppColors.primary, BlendMode.srcIn),
+            // ),
+            Text(
+              'SoldNet',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/app/app_router.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:soldnet/app/app.dart';
+import 'package:soldnet/presentation/pages/home_page.dart';
+import 'package:soldnet/presentation/pages/profile_page.dart';
+
+final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
+
+class ScreenPaths {
+  static String redirection = '/';
+  static String home = '/home';
+  static String profile = '/profile';
+}
+
+final GoRouter router = GoRouter(
+  initialLocation: ScreenPaths.redirection,
+  navigatorKey: rootNavigatorKey,
+  debugLogDiagnostics: kDebugMode,
+  routes: [
+    AppRoute(ScreenPaths.redirection, (_) => const RedirectionPage()),
+    //A route to show bottomNavBar(menu) on main screens
+    ShellRoute(
+      builder: (context, state, child) {
+        return Scaffold(
+          extendBody: true,
+          body: child,
+          // bottomNavigationBar: AppNavBar(),
+        );
+      },
+      routes: [
+        AppRoute(ScreenPaths.home, (_) => HomePage(), noTransition: true),
+        AppRoute(ScreenPaths.profile, (_) => ProfilePage(), noTransition: true),
+      ],
+    ),
+  ],
+);
+
+class AppRoute extends GoRoute {
+  final bool useFade;
+  final bool noTransition;
+  final bool isTransparent;
+
+  AppRoute(
+    String path,
+    Widget Function(GoRouterState s) builder, {
+    List<GoRoute> routes = const [],
+    this.useFade = true,
+    this.noTransition = false,
+    this.isTransparent = false,
+    String? Function(GoRouterState)? routeRedirect,
+    bool fullScreenDialog = false,
+  }) : super(
+          path: path,
+          routes: routes,
+          pageBuilder: (context, state) {
+            final child = builder(state);
+
+            if (isTransparent) {
+              return CustomTransitionPage(
+                opaque: false,
+                barrierDismissible: true,
+                barrierColor: Colors.transparent,
+                child: child,
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+              );
+            }
+
+            if (noTransition) {
+              return NoTransitionPage(key: state.pageKey, child: child);
+            }
+
+            if (useFade && Platform.isAndroid) {
+              return CustomTransitionPage(
+                fullscreenDialog: fullScreenDialog,
+                barrierColor: Colors.white,
+                key: state.pageKey,
+                child: builder(state),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+              );
+            }
+            return CupertinoPage(key: state.pageKey, child: child);
+          },
+        );
+}
