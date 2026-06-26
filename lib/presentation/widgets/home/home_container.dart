d9c3bc95ff7dae276a742137a06801985d13fe0f
oleh <phd.chornyi@gmail.com>
2026-06-26 11:36:22 +0300

    homePage(under construction)

--- lib/presentation/widgets/home/home_container.dart
+++ lib/presentation/widgets/home/home_container.dart
@@ -5,6 +5,54 @@ class HomeContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final paddingTop = MediaQuery.of(context).padding.top;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: paddingTop + 12,
+          ),
+          ListView.separated(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            padding: EdgeInsets.zero,
+            itemCount: homeItems.length,
+            separatorBuilder: (context, index) => const SizedBox(
+              height: 12,
+            ),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(homeItems[index].bgImage)),
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, top: 100),
+            child: Image.asset(
+              'assets/images/redirection_page/kitty.png',
+              height: 200,
+            ),
+          ),
+          const SizedBox(
+            height: 200,
+          ),
+        ],
+      ),
+    );
   }
 }
+
+typedef HomeItemRecord = ({String label, String bgImage});
+
+List<HomeItemRecord> get homeItems => [
+      (label: 'Blah', bgImage: 'assets/images/prints/snake.png'),
+      (label: 'Blah2', bgImage: 'assets/images/prints/zebra.png'),
+      (label: 'Blah3', bgImage: 'assets/images/prints/tiger.png'),
+      (label: 'Blah4', bgImage: 'assets/images/prints/cheeta.png'),
+      (label: 'Blah5', bgImage: 'assets/images/prints/leopard.png'),
+      (label: 'Blah6', bgImage: 'assets/images/prints/leaves.png'),
+    ];
